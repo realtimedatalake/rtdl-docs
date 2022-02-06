55486b0358540bc65a2167313ca15b45e16700ea
Gavin Johnson <gavinj1984@gmail.com>
2022-02-05 18:04:41 -0800

    trying GitHub Pages solution here, https://dev.to/jameswallis/deploying-a-next-js-app-to-github-pages-24pn
    Signed-off-by: Gavin Johnson <gavinj1984@gmail.com>

--- env-config.js
+++
@@ -1,5 +0,0 @@
-// Gavin, 20220205: Added for GitHub Pages compatibility
-const prod = process.env.NODE_ENV === "production"
-module.exports = {
-  'process.env.BACKEND_URL': prod ? '/rtdl-docs' : ''
-}
--- next.config.js
+++ next.config.js
@@ -4,7 +4,10 @@ const withNextra = require('nextra')({
     themeConfig: './theme.config.js',
     // optional: add `unstable_staticImage: true` to enable Nextra's auto image import
 
-    // Gavin, 20220205: Added for GitHub Pages compatibility
-    assetPrefix: prod ? '/rtdl-docs/' : '',
+    // Gavin, 20220205: Start. Added for GitHub Pages compatibility. Comment out for dev builds
+    basePath: '/rtdl-docs',
+    assetPrefix: '/rtdl-docs/',
+    //assetPrefix: prod ? '/rtdl-docs/' : '',
+    // Gavin, 20220205: Start.
 })
 module.exports = withNextra()
